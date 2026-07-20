3c02f18932011ba3c37c84abb71592f8931e8056
jibonergolpo1289 <ra1289zih@gmail.com>
2026-07-20 20:14:18 +0600

    Update categories in data.js with new entries

--- data.js
+++ data.js
@@ -7,7 +7,8 @@
 const CATEGORIES = [
   { slug: "romantic", name: "সৎ আম্মু" },
   { slug: "natok",    name: "ভাবী" },
-  { slug: "golpo",    name: "খালা" }
+  { slug: "golpo",    name: "চাচী" }
+  { slug: "movie",    name: "খালা" }
 ];
 
 // প্রতিটি লেখার জন্য একটি entry। নতুন লেখা যোগ করতে এই প্যাটার্ন কপি করুন
